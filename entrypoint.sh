d3b54e0597baec9f8bd290689a4b3e9b0fdee3f4
discko <discko@qq.com>
2020-10-23 15:44:16 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,9 @@
 
 set -eu
 
-echo "$*"
-
 INPUT_STRIP_COMPONENTS=0
 INPUT_HOST=123
-
+echo "$*"
 
 [ -n "$INPUT_STRIP_COMPONENTS" ] && export INPUT_STRIP_COMPONENTS=$((INPUT_STRIP_COMPONENTS + 0))
 
